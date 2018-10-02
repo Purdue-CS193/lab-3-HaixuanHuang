9934e9a46ccecb9795d3205fdc63618b4d1881ac
Haixuan Huang <hxhuang@purdue.edu>
2018-10-02 17:13:24 -0400

    Update BuggyUtilClient.java

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -3,12 +3,10 @@ public class BuggyUtilClient {
     public static void main(String[] args) {
         // First test case in Main
         System.out.println("Example 1: Make My Username");
-        System.out.println("In: first = " + "\"Sean\"" +
-                ", last = " + "\"Flannery\"");
+        System.out.println("In: first = " + "\"Haixuan\"" +
+                ", last = " + "\"Huang\"");
         System.out.println("Out: " +
-        StringUtils.makePurdueUsername("Sean", "Flannery"));
+        StringUtils.makePurdueUsername("Haixuan", "Huang"));
     }
 }
-
-
 
